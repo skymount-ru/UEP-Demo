422b14dac1e0cc90acc8a4633e1ac34fdad98a48
Dmitry Vikharev <idexdv@gmail.com>
2020-04-01 19:25:55 +0300

    Fix

---
+++ common/models/db/BaseCurrency.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace common\models\db;
+
+use Yii;
+
+/**
+ * This is the model class for table "currency".
+ *
+ * @property int $id
+ * @property string $name
+ * @property float $value
+ */
+class BaseCurrency extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'currency';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'value'], 'required'],
+            [['value'], 'number'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'value' => 'Value',
+        ];
+    }
+}
